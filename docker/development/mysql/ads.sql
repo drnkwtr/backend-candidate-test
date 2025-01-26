bfed3a02737e1d1faf1c7c86ac7d5538f27455b3
drnkwtr <drnkwtr@forblitz.ru>
2025-01-27 02:42:24 +0300

    changed sets to adsets due to eloquent naming rules

--- docker/development/mysql/ads.sql
+++ docker/development/mysql/ads.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `campaigns`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `sets`
+CREATE TABLE IF NOT EXISTS `adsets`
 (
     `id`   BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` INT(11)    NOT NULL
@@ -15,14 +15,15 @@ CREATE TABLE IF NOT EXISTS `sets`
 CREATE TABLE IF NOT EXISTS `ads`
 (
     `id`          BIGINT(20)   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `ad_id`       BIGINT(20)   NOT NULL,
     `name`        VARCHAR(255) NOT NULL,
     `campaign_id` BIGINT(20)   NOT NULL,
-    `set_id`      BIGINT(20)   NOT NULL,
+    `adset_id`    BIGINT(20)   NOT NULL,
     `cost`        DOUBLE       NOT NULL,
     `impressions` INT          NOT NULL,
     `clicks`      INT          NOT NULL,
     `date`        DATE         NOT NULL,
     FOREIGN KEY (`campaign_id`) REFERENCES `campaigns` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (`set_id`) REFERENCES `sets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (`adset_id`) REFERENCES `adsets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
