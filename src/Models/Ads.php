5bdb5a2b74e557b53460a54c20d69f6991080680
drnkwtr <drnkwtr@forblitz.ru>
2025-01-27 03:29:37 +0300

    implemented models with addSingle, addMultiple, removeSingle, updateSingle methods (Ads model, for example)

--- src/Models/Ads.php
+++ src/Models/Ads.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ads extends Model
 {
+    protected $fillable = [
+        'name',
+        'campaign_id',
+        'set_id',
+        'cost',
+        'impressions',
+        'clicks',
+        'date',
+    ];
 
+    public static function addSingle($attributes, $values)
+    {
+        self::query()->updateOrCreate($attributes, $values);
+    }
+
+    public static function addMultiple($attributes, $values)
+    {
+        foreach ($values as $value) {
+            self::addSingle($attributes, $value);
+        }
+    }
+
+    public static function removeSingle($data)
+    {
+        return self::query()->where($data)->delete();
+    }
+
+    public static function updateSingle($data, $updated_data)
+    {
+        return self::query()->where($data)->update($updated_data);
+    }
 }
