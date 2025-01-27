d7ab58282d1dd99f002a4446df9a176ec16011d5
drnkwtr <drnkwtr@forblitz.ru>
2025-01-27 06:39:56 +0300

    finished console command with accepting fileProccessingMode parameter (not sure how it should be use so just added placeholder)

--- src/Console/ImportCsvAds.php
+++ src/Console/ImportCsvAds.php
@@ -2,25 +2,45 @@
 
 namespace App\Console;
 
+use http\Client;
+use Illuminate\Support\Facades\Http;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ImportCsvAds extends Command
 {
+
     protected function configure(): void
     {
         parent::configure();
 
         $this
             ->setName('import-csv-ads')
-            ->setDescription('import-csv-ads command');
+            ->setDescription('import-csv-ads command')
+            ->addArgument('fileProcessingMode', InputArgument::REQUIRED, 'File processing mode');
 
     }
 
     // TODO: Реализовать алгоритм для обработки файла ads.xlsx
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        return 1;
+        $processingMode = $input->getArgument('fileProcessingMode');
+        if ($processingMode) {
+            // Do something
+        } else {
+            // Do something else
+        }
+
+        $url = env('HOST') . '/api/upload-ads';
+
+        $client = new \GuzzleHttp\Client();
+        $responseCode = $client->get($url)->getStatusCode();
+
+        return match ($responseCode) {
+            200 => 1,
+            default => 0,
+        };
     }
 }
